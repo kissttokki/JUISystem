073e1ae46142d4ae8a86b29f9dd675af33800e02
JJaery <papa7545@gmail.com>
2026-07-08 18:10:43 +0900

    change version 1.0.1 -> 1.0.2

--- Editor/EditorExtension.cs
+++ Editor/EditorExtension.cs
@@ -1,6 +1,6 @@
-using System;
 using TMPro;
 using UnityEditor;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -27,7 +27,8 @@ public static class RectTransformExtension
         }
     }
 
-    [MenuItem("GameObject/UI/Create UGUI Text #T", false, 10)]
+    [MenuItem("GameObject/UI/Create UGUI Text", false, 10)]
+    [Shortcut("JUISystem/Create UGUI Text", KeyCode.T, ShortcutModifiers.Shift)]
     private static void CreateTextUnderSelected()
     {
         var selected = Selection.activeGameObject;
@@ -68,7 +69,8 @@ public static class RectTransformExtension
         Selection.activeObject = (textGO);
     }
 
-    [MenuItem("GameObject/UI/Create Sprite #S", false, 10)]
+    [MenuItem("GameObject/UI/Create Sprite", false, 10)]
+    [Shortcut("JUISystem/Create Sprite", KeyCode.S, ShortcutModifiers.Shift)]
     private static void CreateSprite()
     {
         var selected = Selection.activeGameObject;
@@ -100,7 +102,8 @@ public static class RectTransformExtension
         Selection.activeObject = (textGO);
     }
 
-    [MenuItem("GameObject/UI/Create Button #B", false, 10)]
+    [MenuItem("GameObject/UI/Create Button", false, 10)]
+    [Shortcut("JUISystem/Create Button", KeyCode.B, ShortcutModifiers.Shift)]
     private static void CreateButton()
     {
         var selected = Selection.activeGameObject;
@@ -121,6 +124,8 @@ public static class RectTransformExtension
         // Text 오브젝트 생성
         GameObject button = new GameObject("New Button", typeof(RectTransform), typeof(CustomButton), typeof(Image));
         button.transform.SetParent(selected.transform, false);
+        var buttonRect = button.GetComponent<RectTransform>();
+        buttonRect.sizeDelta = new Vector2(200, 100);
 
         // Text 오브젝트 생성
         GameObject textGO = new GameObject("New Text", typeof(RectTransform), typeof(TextMeshProUGUI));
@@ -130,14 +135,12 @@ public static class RectTransformExtension
         var text = textGO.GetComponent<TMP_Text>();
         text.text = "New Text";
         text.color = Color.black;
+        text.rectTransform.anchorMin = Vector2.zero;
+        text.rectTransform.anchorMax = Vector2.one;
+        text.rectTransform.offsetMin = Vector2.zero;
+        text.rectTransform.offsetMax = Vector2.zero;
         text.alignment = TextAlignmentOptions.Midline;
-        text.autoSizeTextContainer = true;
-
-        var rect = textGO.GetComponent<RectTransform>();
-        rect.anchorMin = Vector2.zero;
-        rect.anchorMax = Vector2.one;
-        rect.offsetMin = Vector2.zero;
-        rect.offsetMax = Vector2.zero;
+        //text.autoSizeTextContainer = true;
 
         // 생성된 오브젝트 선택
         Selection.activeObject = (button);
